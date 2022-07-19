4bf93cf3941f05ef2811f36d3305c6cad3a932b2
LeonSaal <68274160+LeonSaal@users.noreply.github.com>
2022-07-19 17:29:56 +0200

    Update .gitignore

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
-    install_requires=["pandas", "matplotlib", "dotmap", "openpyxl", "lxml", "PySimpleGUI", "ipykernel"],
+    install_requires=["pandas", "matplotlib", "openpyxl", "lxml", "PySimpleGUI","xlwt",'odfpy'],
     python_requires=">=3.6",
 )
