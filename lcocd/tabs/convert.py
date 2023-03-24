bf96cd182a8fa0588c6593f27f25f174d94f3502
LeonSaal <leon.saal@posteo.de>
2023-03-24 17:02:29 +0100

    restructure layout

---
+++ lcocd/tabs/convert.py
@@ -0,0 +1,22 @@
+import PySimpleGUI as sg
+from ..lang import lang
+
+## Conversion tab
+# Menu
+menu_definition = [[lang.info, lang.about, lang.settings]]
+
+
+# Conversion tab
+tab_conversion = [
+    [sg.MenuBar(menu_definition, key="-MENU-")],
+    [sg.CBox(lang.incl_sub, default=False, key="-INP_SUBFOLDERS-")],
+    [sg.CBox(lang.skip, default=True, key="-OUT_SKIP-")],
+    [sg.CBox(lang.keep_sub, default=False, key="-OUT_SUBDIR-")],
+    #[],
+    #[sg.VPush()],
+    #[sg.Frame(lang.corr, corrections_frame, expand_x=True)],
+    #[sg.VPush()],
+    #[sg.Frame(lang.int, int_frame, expand_x=True)],
+    #[sg.VPush()],
+    [sg.Button(lang.convert, key="-RUN-", disabled=True), sg.Push()],
+]
